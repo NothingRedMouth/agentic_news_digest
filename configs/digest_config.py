49a30c8e640f7d81da274b68c7b759b9c674cf75
Ilya Sirenko <ilyasirenko95@gmail.com>
2026-06-17 15:24:01 +0300

    feature/pp-155 Added digest config and GBNF grammar

---
+++ configs/digest_config.py
@@ -0,0 +1,52 @@
+TELEGRAM_CHANNELS = [
+    "allbiometrics",
+    "biometria_rf",
+    "ict_moscow_ai",
+]
+
+DAYS_BACK = 7
+MAX_POSTS_PER_CHANNEL = 50
+
+KEYWORDS = [
+    "биометрия",
+    "CV",
+    "распознавание лиц",
+    "видеоаналитика",
+    "DeepFake",
+    "цифровая идентификация",
+    "Liveness",
+    "face recognition",
+    "572-ФЗ",
+    "303-ФЗ",
+    "ЕБС",
+    "NtechLab",
+    "Tevian",
+    "3DiVi",
+    "RecFaces",
+    "VizorLabs",
+    "Vocord",
+    "Macroscop",
+    "Центр Речевых Технологий",
+    "BIOSMART",
+    "Интеллоджик",
+    "Intelligent Security Systems",
+    "MTS AI",
+]
+
+CATEGORIES = [
+    "Биометрия",
+    "Компьютерное зрение",
+    "Видеоаналитика",
+    "DeepFake",
+    "Законодательство",
+    "Безопасность",
+    "Цифровое ID",
+    "Рынок",
+    "Другое",
+]
+
+MAX_TOP_ARTICLES = 10
+MAX_EXTRA_ARTICLES = 10
+MAX_CONCURRENT_FETCHES = 5
+
+STATUS_FILE = "configs/bot_status.json"
